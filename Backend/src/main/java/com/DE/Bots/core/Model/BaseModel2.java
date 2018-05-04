3e2716bfe5b8fe79b04137e2df20d773d5d1962e
manish.singh <manish.kr.singh28@gmail.com>
2018-05-04 13:54:35 +0530

    #Comment:- aaaaaaabbbbaaaaaaaaaa Change-Id: I6a1de4638bcaf7ec0a408ba2293d1961ec116d2c

--- Backend/src/main/java/com/DE/Bots/core/Model/BaseModel2.java
+++ Backend/src/main/java/com/DE/Bots/core/Model/BaseModel2.java
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.Id;
 public class BaseModel2 {
 	@Id
     private ObjectId id;
-	private int aaaaaaaaaaaaa;
+	private int aaaaaaaaaabbbbaaa;
     public ObjectId getId() {
         return id;
     }
